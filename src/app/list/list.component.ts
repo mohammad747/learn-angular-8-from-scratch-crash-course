d0fc4319dbc0b81f5969ab43532709fd18fdc526
mohammad hashemi <mohammad.hashemi747@gmail.com>
2021-07-28 23:52:55 +0430

    add service and use an open api to get the list of brewries

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: any;
+
+  constructor(
+    private httpService : HttpService
+  ) { }
 
   ngOnInit(): void {
+    this.httpService.getBear().subscribe(data => {
+      this.brews = data;
+      console.log(this.brews);
+
+    });
   }
 
 }
